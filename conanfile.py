797f8afa9029871c1162345becc5f80ef20283ba
Steve Robinson <ssrobins@gmail.com>
2021-04-10 22:05:44 -0700

    macOS building

--- conanfile.py
+++ conanfile.py
@@ -6,6 +6,6 @@ class Conan(ConanFile):
 
     def requirements(self):
         #self.requires("openssl/1.1.1j")
-        #self.requires("qt/5.15.2@bincrafters/stable")
+        self.requires("qt/5.15.2@bincrafters/stable")
         #self.requires("qt/6.0.1@bincrafters/stable")
-        self.requires("qt/5.15.2")
+        #self.requires("qt/5.15.2")
